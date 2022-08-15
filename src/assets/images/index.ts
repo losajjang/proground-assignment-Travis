4b8675728c5b8b0ce820c7192c4482377466f470
losajjang <losajjang@naver.com>
2022-08-15 11:45:28 +0900

    feat: footer 개발

---
+++ src/assets/images/index.ts
@@ -0,0 +1,25 @@
+import BackButton from './back_black.png';
+import Cnk from './CNK.png';
+import EstimatedCoin from './estimated_coin.png';
+import Grnd from './GRND.png';
+import Klay from './KLAY.png';
+import LeaderBoard from './Leader board.png';
+import Profile from './profile.png';
+import ScoreA from './score_a.png';
+import Score from './score.png';
+import UserProfile from './user_profile.png';
+import Wallet from './wallet.png';
+
+export {
+  BackButton,
+  Cnk,
+  EstimatedCoin,
+  Grnd,
+  Klay,
+  LeaderBoard,
+  Profile,
+  ScoreA,
+  Score,
+  UserProfile,
+  Wallet,
+};
